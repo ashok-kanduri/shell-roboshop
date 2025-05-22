f1160d79fe5d5181a7c39cb24cf4d203345a4752
ashokkumar <ashkanduri@gmail.com>
2025-05-22 16:53:06 +0530

    make the changes

--- mongodb.sh
+++ mongodb.sh
@@ -8,26 +8,28 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
-echo script started executing at $(date) | tee -a $LOG_FILE
+echo "script started executing at $(date)" | tee -a $LOG_FILE
 
+#check if the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then 
-    echo -e "$R ERROR:: please access using root access $N" | tee -a $LOG_FILE
+    echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
     exit 1
 else 
     echo "you are running with root access" | tee -a $LOG_FILE
 fi
 
+#validate functions take input as exit status for the command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then 
-        echo -e "$2 is $G SUCCESSFUL $N" | tee -a $LOG_FILE
+        echo -e "$2 is .... $G SUCCESSFUL $N" | tee -a $LOG_FILE
     else 
-        echo -e "$2 is $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 is .... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -45,7 +47,7 @@ systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "starting mongodb"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALIDATE $? "editing mongodb conf file for remote access"
+VALIDATE $? "editing mongodb-conf file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "restarting mongodb"
