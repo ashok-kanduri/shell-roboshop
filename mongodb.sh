7d5298227b7e4111c8a28a6294913a49acb6937a
ashokkumar <ashkanduri@gmail.com>
2025-05-22 16:17:59 +0530

    make the changes

---
+++ mongodb.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=(id -u)
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+
+LOGS_FOLDER=/var/log/robshop-logs
+SCRIPT_NAME=(echo $0 | cut -d "." -f1)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo script started executing at $(date) | tee -a $LOG_FILE
+
+if [ USERID -ne 0 ]
+then 
+    echo -e "$R ERROR:: please access using root access $N" | tee -a $LOG_FILE
+    exit 1
+else 
+    echo "you are running with root access" | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2 is $G SUCCESSFUL $N" | tee -a $LOG_FILE
+    else 
+        echo -e "$2 is $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "installing mongodb server"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "enabling mongodb"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "starting mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "editing mongodb conf file for remote access"
+
+systectl restart mongod &>>$LOG_FILE
+VALIDATE $? "restarting mongodb"
